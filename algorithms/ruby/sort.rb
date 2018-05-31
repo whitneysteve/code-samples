3525443344b288e384b76aee959168133c0d1046
Stephen Whitney <steve.whitney@gmail.com>
2018-05-31 14:12:58 +0100

    Quick and merge sort for ruby

--- algorithms/ruby/sort.rb
+++ algorithms/ruby/sort.rb
@@ -7,7 +7,7 @@ module Sort
   # Perform a bubble sort on an array. Sort is performed in place and
   # assumes conflict ordering is not important.
   def bubble_sort(arr)
-    arr.each_with_index { |_,i|
+    arr.each_with_index { |_, i|
       arr.each_with_index { |_, j|
         swap(arr, i, j) if arr[i] < arr[j]
       }
@@ -17,25 +17,48 @@ module Sort
   # Perform an insertion sort on an array. Sort is performed in place and
   # assumes conflict ordering is not important.
   def insertion_sort(arr)
-    arr.each_with_index { |_,i|
+    arr.each_with_index { |_, i|
       j = 0
-
       while j < i do
-        if arr[j] > arr[i]
+        if arr[i] < arr[j]
           reposition(arr, i, j)
           break
         end
-        j += 1;
+        j += 1
       end
     }
+  end
+
+  # Perform a merge sort on an array. Sort is performed in another array
+  # and returned. In and order conflict where items are equal the original
+  # ordering remains.
+  def merge_sort(arr)
+    if arr.size < 2 || (arr.size == 2 && arr[0] < arr[1])
+      arr
+    elsif arr.size == 2
+      [arr[1], arr[0]]
+    else
+      mid = arr.size / 2
+      first = arr.slice(0, mid)
+      second = arr.slice(mid, arr.size)
+      merge(merge_sort(first), merge_sort(second))
+    end
+  end
+
+  # Perform a quick sort on an array. Sort is performed in place and
+  # assumes conflict ordering is not important.
+  def quick_sort(arr, low=0, high=arr.size - 1)
+    if low < high
+      partition_index = partition(arr, low, high)
+      quick_sort(arr, low, partition_index - 1)
+      quick_sort(arr, partition_index + 1, high)
+    end
   end
 
   # Perform a selection sort on an array. Sort is performed in place and
   # assumes conflict ordering is not important.
   def selection_sort(arr)
-    arr.each_with_index { |_,i|
-      j = i
-
+    arr.each_with_index { |_, i|
       least = MAX_FIXNUM
       least_index = i
       for j in i .. (arr.length - 1) do
@@ -45,15 +68,53 @@ module Sort
           least_index = j
         end
       end
-
-      if i != least_index
+      if least_index != i
         swap(arr, i, least_index)
       end
-
     }
   end
 
   private
+  # Merge two arrays in ascending order.
+  def merge(first, second)
+    i = 0
+    j = 0
+    result = []
+    while (i < first.size && j < second.size) do
+      first_val = first[i]
+      second_val = second[j]
+      if first_val < second_val
+        result.push(first_val)
+        i += 1
+      else
+        result.push(second_val)
+        j += 1
+      end
+    end
+
+    result.concat(first.slice(i, first.size))
+    result.concat(second.slice(j, second.size))
+
+    result
+  end
+
+  # Partition a sub array or arr, denoted by the bounding indexes low and high.
+  def partition(arr, low, high)
+    pivot = arr[high]
+    pivot_index = low
+
+    for j in pivot_index .. (high) do
+      if arr[j] < pivot
+        swap(arr, j, pivot_index)
+        pivot_index += 1
+      end
+    end
+
+    swap(arr, pivot_index, high)
+
+    pivot_index
+  end
+
   # Reposition an element from index `from` to index `to`.
   def reposition(arr, from, to)
     arr.insert(to, arr.delete_at(from))
@@ -84,5 +145,11 @@ if __FILE__ == $0
   insertion_sort_vals = Marshal.load(Marshal.dump(values))
   insertion_sort insertion_sort_vals
   puts insertion_sort_vals.join(",")
+
+  puts merge_sort(values).join(",")
+
+  quick_sort_vals = Marshal.load(Marshal.dump(values))
+  quick_sort quick_sort_vals
+  puts quick_sort_vals.join(",")
 
 end
